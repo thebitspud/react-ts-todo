74b47384feeca9c9b456ac6502db6bffbc074199
James Zhong <thebitspud@gmail.com>
2021-06-17 15:44:16 -0700

    Added task list display
    + Made UI actually look decent
    + only allow unique task names

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [name, setName] = useState<string>("");
   const [desc, setDesc] = useState<string>("");
   const [date, setDate] = useState<string>(new Date().toLocaleDateString());
-  const [color, setColor] = useState<string>("#ff8000");
+  const [color, setColor] = useState<string>("#ff8080");
   const [taskList, setTaskList] = useState<TaskData[]>([]);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -40,6 +40,13 @@ function App() {
     // Task name is mandatory, other fields optional
     if (name === "") return;
 
+    // Do not create tasks with duplicate names
+    let getMatches = taskList.filter((task) => task.name === name);
+    if (getMatches.length !== 0) {
+      setName("");
+      return;
+    }
+
     const nextTask: TaskData = {
       name: name,
       desc: desc,
@@ -56,10 +63,19 @@ function App() {
     setDesc("");
   };
 
+  const completeTask = (nameToDelete: string): void => {
+    setTaskList(taskList.filter((task) => task.name !== nameToDelete));
+  };
+
   return (
     <div className="App">
-      <h1 className="header">My To-do list</h1>
+      <div id="task-list">
+        {taskList.map((task: TaskData, key: number) => {
+          return <Task key={key} task={task} onComplete={completeTask} />;
+        })}
+      </div>
       <div id="next-item">
+        <h1 className="header">Add Task</h1>
         <input
           type="text"
           name="name"
@@ -90,11 +106,6 @@ function App() {
           />
           <button onClick={addTask}>Add Item</button>
         </div>
-      </div>
-      <div id="task-list">
-        {taskList.map((task: TaskData, key: number) => {
-          return <Task key={key} task={task} />;
-        })}
       </div>
     </div>
   );
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { TaskData } from "../types";
+import "../styles/Task.css";
 
 type TaskProps = {
   task: TaskData;
+  onComplete(nameToDelete: string): void;
 };
 
-const Task = (props: TaskProps) => <div>Amogus</div>;
+const Task = (props: TaskProps) => (
+  <div className="Task" style={{ borderRightColor: props.task.color }}>
+    <h2 className="header" style={{ margin: 0 }}>
+      {props.task.name}
+    </h2>
+    <div>{props.task.desc}</div>
+    <br />
+    <div>Due: {props.task.date}</div>
+    <div>Card Color: {props.task.color}</div>
+    <button className="delete" onClick={() => props.onComplete(props.task.name)}>
+      &#x2715;
+    </button>
+  </div>
+);
 
 export default Task;
